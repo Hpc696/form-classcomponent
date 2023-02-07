d018ce187261f06a5ff90a7f3926bdd90802fef3
Matteo_G <matteo.gonzi99@gmail.com>
2023-02-07 01:20:51 +0100

    feat(Contacts): Done 'Add NewContact'
    fetch method post...

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -26,8 +26,40 @@ class Contacts extends Component{
         }
     }
 
-    render(){
+    addC = async (add) => { add.preventDefault()
+        
+        const newContact = {
+            nome: this.state.nome,
+            cognome: this.state.cognome,
+            telefono: this.state.telefono
+        }
     
+        await fetch("http://localhost:3001/contacts",{
+            method: "POST",
+            headers: { "Content-Type" : "application/json" },
+            body: JSON.stringify(newContact)
+        
+        }).then((res) => res.json())
+
+        .then((res) => {
+
+            this.setState({
+                contatt: [...this.state.contatt, res],
+                nome:"",
+                cognome:"",
+                telefono:""
+            })
+            
+        }).catch((error) => console.log(error))
+
+    }
+
+    render(){
+        const {
+            nome,
+            cognome,
+            telefono
+        } = this.state
         return(
             <>
                 <button onClick={this.showB}>{this.state.check ? "Nascondi" : "Mostra "} Rubrica</button>
@@ -40,7 +72,7 @@ class Contacts extends Component{
                     <tbody>
                     { //console.log(this.state.contatt)
                      this.state.contatt.map( c => ( 
-                        <tr key={c.telefono}>
+                        <tr key={c.id}>
                          <td>{c.nome}</td>
                          <td>{c.cognome}</td>
                          <td>{c.telefono}</td>
@@ -52,6 +84,12 @@ class Contacts extends Component{
                     </tbody>
                 </table>
                 : <></>}
+                <form onSubmit={this.addC}>
+                    <input type="text" value={nome} onChange={a => this.setState( {nome: a.target.value } )} placeholder="Inserisci nuovo nome"></input>
+                    <input type="text" value={cognome} onChange={a => this.setState( {cognome: a.target.value } )} placeholder="Inserisci cognome"></input>
+                    <input type="number" value={telefono} onChange={a=> this.setState( {telefono: a.target.value } )} placeholder="Inserisci telefono"></input>
+                    <button type="submit" >Aggiungi</button>
+                </form>
             </>
         )
     }
